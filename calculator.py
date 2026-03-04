0c759d93c79d269a9e1894490bf4ed767da7c9d3
Boris Arutinov <borinskii242@gmail.com>
2026-03-04 19:03:04 +0200

    feat: add addition and subtraction to calculator functionality

---
+++ calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, a, b):
+        #sum
+        return a + b
+
+    def subtract(self, a, b):
+        #difference
+        return a - b
